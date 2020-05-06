c11395aacf0051a1b7df9c7f9c35b684c5b15909
Vitor Maia <vlma@cin.ufpe.br>
2020-05-06 12:21:57 -0300

    implement route and controller to create consultant

--- api/controllers/consultant.js
+++ api/controllers/consultant.js
@@ -1,6 +1,16 @@
+const { insertConsultant } = require('../models/consultant');
+
 exports.createConsultantAction = [
   (req, res, next) => {
-    
+    const { username, password } = req.body;
+    insertConsultant({ username, password })
+      .then(result => {
+        res.status(200).json({ success: true, data: { consultant: result }})
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ success: false, message: "Erro ao criar consultor."})
+      })
   }
 ]
 
--- api/models/consultant.js
+++ api/models/consultant.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.objectId;
+const bcrypt = require('bcrypt');
 
 const ConsultantSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     required: true,
@@ -14,13 +16,33 @@ const ConsultantSchema = new mongoose.Schema({
 
  
 ConsultantSchema.methods.validPassword = async (candidate, hash) => bcrypt.compare(candidate, hash)
-ConsultantSchema.methods.hashPassword = async (password) =>  bcrypt.hash(password, 10);
+ConsultantSchema.methods.hashPassword = async (password) => bcrypt.hash(password, 10);
 
-const ConsultantModel = mongoose.model(ConsultantSchema);
+const ConsultantModel = mongoose.model('Consultant', ConsultantSchema);
 
-const insertUser = async () => {
+exports.insertConsultant = ({ username, password }) => new Promise( async (resolve, reject) => {
+  // TODO: Verificar se username ainda não foi usado...
+  
+  let consultant = new ConsultantModel({ username, password })
+  try {
+    consultant.password = await consultant.hashPassword(password);
+  } catch(err) {
+    console.log('Erro ao fazer hash da senha.')
+    reject(err)
+    return;
+  }
+  consultant.save()
+    .then((result) => {
+      resolve(result);
+    })
+    .catch(err => {
+      reject(err)
+    })
+})
 
-}
+exports.getConsultant = (username) => new Promise((resolve, reject) => {
+  ConsultantModel.find({ username })
+    .then(data => resolve(data))
+    .catch(err => reject(err))
+})
 
-
-module.exports = ConsultantModel;
