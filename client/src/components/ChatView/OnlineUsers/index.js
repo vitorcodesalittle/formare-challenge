adaf9e566d0110c22ef09989b85b7b88b8874495
Vitor Maia <vlma@cin.ufpe.br>
2020-05-07 17:31:02 -0300

    add layout of online users container

--- client/src/components/ChatView/OnlineUsers/index.js
+++ client/src/components/ChatView/OnlineUsers/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-
-const OnlineUsers = props => {
+import React, { useState } from 'react';
+import './OnlineUsers.css';
+const OnlineUsers = function (props) {
+  const [ isOpened, setIsOpened ] = useState(true);
   return (
-    <div>
-      <p>Usuários Online</p>
-      { props.users.map((u, idx) => u && <p key={idx}>{u.username}</p>)}
+    <div className="OnlineUsers">
+      { props.users.map((u, idx) => u && 
+        <div key={idx} 
+          className='user-container'>
+          <p>{u.username}</p>
+          <div className="online-indicator"/>
+        </div>
+        )
+      }
     </div>
   )
 }
